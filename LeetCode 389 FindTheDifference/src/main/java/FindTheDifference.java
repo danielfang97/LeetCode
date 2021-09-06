182c0c78138337cb4b6f4036cc430ad66bd40b18
Daniel Fang <danielf@eng.utah.edu>
2021-09-06 11:33:30 -0600

    updated leetcode 389 from set to map

--- LeetCode 389 FindTheDifference/src/main/java/FindTheDifference.java
+++ LeetCode 389 FindTheDifference/src/main/java/FindTheDifference.java
@@ -1,14 +1,25 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class FindTheDifference {
 
     public char findTheDifference(String s, String t) {
-        HashSet<Character> stringS = new HashSet<Character>();
-        for (char c: s.toCharArray()) {
-            stringS.add(c);
+        HashMap<Character, Integer> stringS = new HashMap<Character, Integer>();
+        for (char c : s.toCharArray()) {
+            if (stringS.containsKey(c)) {
+                stringS.put(c, stringS.get(c)+1);
+            } else {
+                stringS.put(c, 0);
+            }
         }
-        for (char c: t.toCharArray()) {
-            if (!stringS.contains(c)) {
+        for (char c : t.toCharArray()) {
+            if (stringS.containsKey(c)) {
+                if (stringS.get(c) > 1) {
+                    stringS.put(c, stringS.get(c)-1);
+                } else {
+                    stringS.remove(c);
+                }
+            } else {
                 return c;
             }
         }
